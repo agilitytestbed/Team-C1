d139b404fc1c79730a473ab527171d762065efa1
arsleutjes <arwin.sleutjes@gmail.com>
2018-08-06 14:22:10 +0200

    Improve session_id filter

--- src/main/java/nl/utwente/ing/authorization/SessionIdFilter.java
+++ src/main/java/nl/utwente/ing/authorization/SessionIdFilter.java
@@ -13,8 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static javax.servlet.http.HttpUtils.parseQueryString;
-
 @Component
 public class SessionIdFilter extends GenericFilterBean {
     private static final String AUTHENTICATION_HEADER = "X-Session-ID";
@@ -35,20 +33,14 @@ public class SessionIdFilter extends GenericFilterBean {
         if (httpServletRequest.getRequestURI().equals(httpServletRequest.getContextPath() + SESSIONS_PATH) && ((HttpServletRequest) servletRequest).getMethod().equals(METHOD_POST)) {
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
-            String id = httpServletRequest.getHeader(AUTHENTICATION_HEADER);
-            String urlQuery = httpServletRequest.getQueryString();
-            String[] queryId;
-            if (urlQuery != null) {
-                queryId = parseQueryString(httpServletRequest.getQueryString()).getOrDefault("session_id", null);
-            } else {
-                queryId = null;
-            }
+            String headerId = httpServletRequest.getHeader(AUTHENTICATION_HEADER);
+            String urlId = httpServletRequest.getParameter("session_id");
 
-            if (id != null && this.sessionService.verifyById(id)) {
-                SecurityContextHolder.getContext().setAuthentication(new SessionIdAuthentication(id, true));
+            if (headerId != null && this.sessionService.verifyById(headerId)) {
+                SecurityContextHolder.getContext().setAuthentication(new SessionIdAuthentication(headerId));
                 filterChain.doFilter(servletRequest, servletResponse);
-            } else if (queryId != null && this.sessionService.verifyById(queryId[0])) {
-                SecurityContextHolder.getContext().setAuthentication(new SessionIdAuthentication(queryId[0], true));
+            } else if (urlId != null && this.sessionService.verifyById(urlId)) {
+                SecurityContextHolder.getContext().setAuthentication(new SessionIdAuthentication(urlId));
                 filterChain.doFilter(servletRequest, servletResponse);
             } else {
                 httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Session ID missing or invalid.");
